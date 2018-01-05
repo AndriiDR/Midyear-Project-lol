0e0c2bb9c6d192d1223e4be482620a59ed0305a5
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-01-04 20:33:43 -0500

    Add files via upload

---
+++ LevelSelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+	public Canvas LevelSelectCanvas;
+
+	void Awake() {
+	//for the situation where we need more canvases
+	}
+
+	public void LevelOneOn() {
+		SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+	}
+	public void LevelTwoOn() {
+		//SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+	}
+	public void LevelThreeOn() {
+		//SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+	}
+	public void BackOn() {
+		SceneManager.LoadScene("TitleScreen", LoadSceneMode.Single);
+	}
+
+
+}
