6d8b12719107dbb5efcb669c7dffac03e0b1133c
AndriiDR <32248907+AndriiDR@users.noreply.github.com>
2018-01-08 10:20:40 -0500

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    GameObject[] red =GameObject.FindGameObjectsWithTag("Red");
+    GameObject[] blue = GameObject.FindGameObjectsWithTag("Blue");
+    GameObject[] green = GameObject.FindGameObjectsWithTag("Green");
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
