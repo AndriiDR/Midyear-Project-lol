13af23cb064f809e9e190181127fb946777e3195
salongaale <salongaale@gmail.com>
2018-01-17 22:55:29 -0500

    Add files via upload

---
+++ Spring.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+	private Player player;
+
+	public GameManager gm;
+
+	private GameObject spring;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		JumpSpring(
+	}
+
+	private void JumpSpring(Collider2D other){
+		if (other.gameObject.CompareTag("Player"){
+			player.rb.AddRelativeForce(Vector2.up * 1100f);
+		}
+	}
+}
