80cff5a8e2c245da9852fce3d6798ccaadd9be7c
GrimmAuxiliatrix <ujala601@gmail.com>
2018-01-17 22:37:28 -0500

    Add files via upload

---
+++ SmoothFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject bg; //background
+	private Vector3 offset;
+	private Vector3 bgoffset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+		bgoffset = bg.transform.position - transform.position;
+	}
+	
+	void Update () {
+		
+	}
+	
+	void LateUpdate(){
+		transform.position = player.transform.position + offset;
+		bg.transform.position = transform.position + bgoffset;
+	}
+}
