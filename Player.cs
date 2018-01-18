631088ee7ec8566d2dfa5ac0e95ef2ee63a8366e
salongaale <salongaale@gmail.com>
2018-01-18 00:10:30 -0500

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -13,11 +13,18 @@ public class Player : MonoBehaviour
 
     private float maxSpeed;
 
+    private bool possible;
+    private bool carry;
+
+    private GameObject contact;
+
 
     // Use this for initialization
     void Start()
     {
         maxSpeed = 5f;
+        possible = false;
+        carry = false;
         //anim = GetComponent<Animator>();
     }
 
@@ -35,30 +42,14 @@ public class Player : MonoBehaviour
             rb.AddRelativeForce(Vector2.left * 30f);
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        if (Input.GetKey(KeyCode.W) && rb.velocity.y.Equals(0f))
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y.Equals(0f))
         {
             //anim.SetBool("Jump", true);
             rb.AddRelativeForce(Vector2.up * 450f);
+			if(Input.GetKeyDown(KeyCode.Space){
+				rb.AddRelativeForce(Vector2.up * 450f);
+			}
         }
-
-        if (rb.velocity.y.Equals(0f))
-        {
-            //anim.SetBool("Jump", false);
-        }
-        else
-        {
-            //anim.SetBool("Jump", true);
-        }
-
-        if (rb.velocity.x != 0f /*&& anim.GetBool("Jump").Equals(false)*/)
-        {
-            //anim.SetBool("Run", true);
-        }
-        if (rb.velocity.x == 0f)
-        {
-            //anim.SetBool("Run", false);
-        }
-
 
         //Air physics
         if (Input.GetKey(KeyCode.D) && !(rb.velocity.y.Equals(0f)))
@@ -79,15 +70,65 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        //Trigger death and restart level 
-        if (other.gameObject.CompareTag("Enemy"))
+        //Actions
+        //Throws
+        if (Input.GetKeyDown(KeyCode.S) && carry == true)
         {
-            gm.Death();
+            contact.GetComponent<Knuckles>().Thrown();
+            if (transform.localScale.x == 1)
+            {
+                contact.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * 1000f);
+            }
+            else
+            {
+                contact.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.left * 1000f);
+            }
+            contact = null;
+            carry = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && Input.GetKey(KeyCode.A) && carry == true)
+        {
+            contact.GetComponent<Knuckles>().Thrown();
+            contact.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * 1000f);
+            contact = null;
+            carry = false;
         }
 
+        //Pick Up
+        if (Input.GetKeyDown(KeyCode.S) && possible == true && contact != null)
+        {
+            print("ok");
+            contact.GetComponent<Knuckles>().Lifted(this.gameObject);
+            possible = false;
+            carry = true;
+        }
+
+
+        
+
+
+
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            print("Not ok");
+            gm.Death();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Safe") && carry == false)
+        {
+            possible = true;
+            contact = other.transform.parent.gameObject;
+        }
+        
+    }
+
+    
 }
