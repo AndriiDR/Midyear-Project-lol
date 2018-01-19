7e2e3cc21499d6239e4ab921557db5c09bf526ec
alawngnome <32248903+alawngnome@users.noreply.github.com>
2018-01-19 03:03:42 -0500

    Update Door3.cs

--- Door3.cs
+++ Door3.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door3 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public Canvas VictoryCanvas;
+
+	void Start () {	}
+	void Update () {}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag("Player")){
+			VictoryCanvas.enabled = true;
+		}
 	}
 }
